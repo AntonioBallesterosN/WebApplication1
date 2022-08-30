999d3c1d942425a9b10dd97b8398285217d32541
Alberto Vega <vega-alberto@live.com.mx>
2022-08-29 23:30:50 -0500

    cambios

---
+++ Controllers/JuegosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JuegosController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> ObtenerJuegos()
+        {
+            List<string> juegos = new List<string>()
+            {
+                "Halo", "Doom", "AoE", "Oblivion"
+            };
+
+            return juegos;
+        }
+    }
+}
